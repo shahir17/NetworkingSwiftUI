d543a67d354dfdb4612ab8a434c7b4451b4ede6c
Ahmad Shahir Abdul Satar <abdulsataas2@vcu.edu>
2021-07-08 10:49:46 -0400

    Initial Commit

---
+++ NetworkingSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NetworkingSwiftUI
+//
+//  Created by Ahmad Shahir Abdul Satar on 7/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NetworkingSwiftUI/NetworkingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingSwiftUIApp.swift
+//  NetworkingSwiftUI
+//
+//  Created by Ahmad Shahir Abdul Satar on 7/8/21.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
